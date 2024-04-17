a7d2254d6184c5bbc32ca8da2bf4ce7472156130
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-17 21:45:38 +0200

    id added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,11 @@
-import testData from "../testData.json";
-
-export default function Contact({id, name , phoneNumber }) {
+export default function Contact({ id, name, phoneNumber }) {
   return (
     <li key={id}>
-      <p>{name[0]} {name[1]}</p>
+      <p>
+        {name[0]} {name[1]}
+      </p>
       <p>{phoneNumber}</p>
+      <button>Delete</button>
     </li>
   );
 }
--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -3,7 +3,12 @@ import testData from "../testData.json";
 
 export default function ContactList() {
   const contactList = testData.map(({ name, surname, phoneNumber, id }) => (
-    <Contact name={[name, surname]} phoneNumber={phoneNumber} key={id} id={id} />
+    <Contact
+      name={[name, surname]}
+      phoneNumber={phoneNumber}
+      key={id}
+      id={id}
+    />
   ));
   return <ul>{contactList}</ul>;
 }
