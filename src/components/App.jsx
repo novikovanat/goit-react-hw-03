a904d2566d905449c28966dd3d6763e79d606b7a
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-04-19 21:57:01 +0200

    form validation added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
+
 import ContactForm from "./contactForm/ContactForm";
 import SearchBox from "./searchBox/SearchBox";
 import ContactList from "./contactList/ContactList";
@@ -8,14 +9,14 @@ import testData from "./testData.json";
 const App = () => {
   const [searchFilter, setSearchFilter] = useState("");
   const [userContacts, setUserContacts] = useState(testData);
- 
+
   const shownContacts = userContacts.filter((contact) =>
     contact.name.toLowerCase().includes(searchFilter.toLowerCase())
   );
 
   function handleSubmit(newContact, actions) {
-    newContact.id = nanoid(5);
-    setUserContacts((allContacts)=>[...allContacts, newContact]);
+    newContact.id = nanoid(15);
+    setUserContacts((allContacts) => [...allContacts, newContact]);
     actions.resetForm();
   }
   return (
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,9 +1,18 @@
-import { Formik, Field, Form} from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { useId } from "react";
-
+import * as Yup from "yup";
 export default function LoginForm({ handleSubmit }) {
   const nameFieldId = useId();
   const numberFieldId = useId();
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    phoneNumber: Yup.string()
+      .matches(/^[1-9]\d{1,14}$/, "Invalid phone number")
+      .required("Phone number is required"),
+  });
   return (
     <div>
       <Formik
@@ -13,12 +22,19 @@ export default function LoginForm({ handleSubmit }) {
           id: "",
         }}
         onSubmit={handleSubmit}
+        validationSchema={validationSchema}
       >
         <Form>
-          <label htmlFor={nameFieldId}>Name</label>
-          <Field id={nameFieldId} name="name" />
-          <label htmlFor={numberFieldId}>Phone number</label>
-          <Field id={numberFieldId} name="phoneNumber" type="number" />
+          <div>
+            <label htmlFor={nameFieldId}>Name</label>
+            <Field id={nameFieldId} name="name" />
+            <ErrorMessage name="name" component="span" />
+          </div>
+          <div>
+            <label htmlFor={numberFieldId}>Phone number</label>
+            <Field id={numberFieldId} name="phoneNumber" />
+            <ErrorMessage name="phoneNumber" component="span" />
+          </div>
           <button type="submit">Add contact</button>
         </Form>
       </Formik>
